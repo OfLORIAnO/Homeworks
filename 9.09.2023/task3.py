64a9e171c662cfc01ef8e396e0479cdc4350f857
fLORIAN <71528743+OfLORIAnO@users.noreply.github.com>
2023-09-10 17:41:49 +0300

    refactoring

--- 9.09.2023/task3.py
+++ 9.09.2023/task3.py
@@ -2,23 +2,32 @@ def printTable(table):
     cnt = 0
     for row in table:
         cnt += 1
-        print(cnt, row)
+        print(row)
 def createTable(row): #? Создаём таблицу
     table = []
     for _ in range(row):
-        table.append(" "*20)
+        table.append(" ")
     return table
 def inputData():
     word = input("Введите слово: ")
     row = int(input("Введите кол-во строк: "))
-    
     return word, row
-def addLetter(table, posY, posX, letter): #? Меняем значение символа на заданной клетке
+def addRightCell(table):#Расширяем таблицу вправо
+    tempTable = []
+    for row in table:
+        row += " "
+        tempTable.append(row)
+    return tempTable
+
+def addLetter(table, posY, posX, letter, mode): #? Меняем значение символа на заданной клетке
+    if mode == 'diag': #? Если мы идём по диагонали, то расширяем таблицу вправо
+        table = addRightCell(table) 
     row = table[posY]
     rowTemp = []
     for symbol in row:
         rowTemp.append(symbol)
     row = rowTemp
+    
     row[posX] = letter
     table[posY] = "".join(row)
     return table
@@ -35,7 +44,7 @@ def Program():
         letter = word[i]
         if mode == "vert": #? Если вертикальный режим, то 
             cntY -= 1
-            table = addLetter(table, posY, posX, letter)
+            table = addLetter(table, posY, posX, letter, mode)
             if cntY == 0: #? Если мы дошли до нижней ячейки
                 mode = "diag" #? меняем на diag
                 posY -= 1 #? Прыгаем на клетку наверх
@@ -45,7 +54,7 @@ def Program():
                 posY += 1 #? Прыгаем на клетку вниз
         else:
             cntY -= 1 
-            table = addLetter(table, posY, posX, letter)
+            table = addLetter(table, posY, posX, letter, mode)
             if cntY == 0: #? Если мы дошли до верхнй ячейки
                 mode = "vert" #? меняем на vert
                 posY += 1 #? Прыгаем на клетку вниз
