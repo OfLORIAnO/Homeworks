c463baba56b7f088175fac6f2594a5d09fad694e
fLORIAN <71528743+OfLORIAnO@users.noreply.github.com>
2023-09-23 17:14:43 +0300

    1.0

--- 16,09,2023/task3.py
+++ 16,09,2023/task3.py
@@ -1,3 +1,6 @@
+from task1 import task1
+
+
 def copyElemInNewArray(array):
     newArr = []
     for e in array:
@@ -6,10 +9,12 @@ def copyElemInNewArray(array):
 
 
 def deleteFromArray(array, leftInd, rigthInd):
-    newArr = []
+    newArr = array
     for i in range(len(array)):
-        if i != leftInd and i != rigthInd:
-            newArr.append(array[i])
+        if i == leftInd:
+            newArr[i] = ""
+        elif i == rigthInd:
+            newArr[i] = ""
     return newArr
 
 
@@ -27,30 +32,66 @@ def convertStringToArray(str):
     return mass
 
 
-def bracket(s):
+def isEmpty(array):
+    cnt = 0
+    for i in range(len(array)):
+        if array[i] == "":
+            cnt += 1
+    return cnt == len(array)
+
+
+def checkUncorrect(array, mass):
+    newMass = [" "] * (len(mass) + 1)
+    for i in range(len(array)):
+        if array[i] == "":
+            newMass[i] = mass[i]
+        else:
+            newMass[i] = " "
+    newMass = "".join(newMass).split()
+    tempString = ""
+    for elem in newMass:
+        if len(elem) >= len(tempString):
+            tempString = elem
+    if len(tempString) > 0:
+        return tempString
+    else:
+        return False
+
+
+def bracket(s): #! Основная функция
     mass = convertStringToArray(s)
-    l = 0
-    r = l + 1
+    l, r, flag = 0, 1, False
     newArr = copyElemInNewArray(mass)
-    flag = False
-    while len(newArr) > 0:
+    while not (isEmpty(newArr)):  # ? Пока в списке есть скобки
         lElem = newArr[l]
         rElem = newArr[r]
-        if getPair(lElem, rElem):
+        if getPair(lElem, rElem):  # ? Если есть пара
             newArr = deleteFromArray(newArr, l, r)
             flag = True
-        l += 1
-        r = l + 1
-        if l >= (len(newArr) - 2) or len(newArr) == 2:
-            l = 0
+        elif rElem == "":  # ? Если правый символ пустой, то двинуться вправо
+            r += 1
+            if r >= len(newArr):  # ? Если выходим за границы списка
+                l, r = 0, 1  # ? Начинаем сначала
+                if flag == False:  # ? Если не было ни одной пары в списке
+                    return checkUncorrect(newArr, mass)
+                flag = False
+            continue
+        else:  # ? Иначе двигаемся вперёд
+            l += 1
             r = l + 1
-            if flag == False:
-                return False
+        if l >= len(newArr) or r >= len(newArr) - 1:  # ? Если дошли до конца списка
+            l, r = 0, 1  # ? начинаем сначала
+            if flag == False:  # ? Если не было ни одной пары в списке
+                return checkUncorrect(newArr, mass)
             flag = False
     return True
 
 
 if __name__ == "__main__":
-    print(bracket("{}()[{}]"))
-    print(bracket("{}[][][]()()()()()[{}]"))
-    print(bracket("{[(]){[()]}}"))
+    print("1 ", bracket("{}{}{}{[{}()][()[]][()()]}"))  #! True
+    print("2 ", bracket("{{{{}}}{{{}}}}}"))  #! False {{{{}}}{{{}}}}
+    print("3 ", bracket("{}()[{}]"))  #! True
+    print("4 ", bracket("{}[][][]()()()({)(})[{}]"))  #! False {}[][][]()()()
+    print("5 ", bracket("{[(]){[()]}}"))  #! False {[()]}
+    print("6 ", bracket("())(())))"))  #! False (())
+    print("7 ", bracket("}{)("))  #! False
