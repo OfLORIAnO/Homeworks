37cea08736084e27db974fb052f79b8ae3c84605
Florian <71528743+OfLORIAnO@users.noreply.github.com>
2024-01-22 23:17:13 +0300

    в процессе второй

--- chapter 8/task2.py
+++ chapter 8/task2.py
@@ -15,7 +15,6 @@ styles = {"Сплошная": "-", "Пунктир": "--", "Штрихпункт
 
 
 def tangent_line_coefficients(a, b, delta, t):
-    # Вычисление углового коэффициента касательной в точке (x, y)
     x, y = lissajous(a, b, delta, t)
     dx_dt = a * np.cos(a * t + delta)
     dy_dt = b * np.cos(b * t)
@@ -25,7 +24,7 @@ def tangent_line_coefficients(a, b, delta, t):
 
 
 def update_graph(val):
-    global point, tangent_line
+    global point, tangent_line, delta, show_tangent,x_values, y_values
     t = slider_t.val
     x, y = lissajous(a, b, delta, t)
 
@@ -34,7 +33,7 @@ def update_graph(val):
     if show_tangent:
         slope, intercept = tangent_line_coefficients(a, b, delta, t)
         tangent_line.set_data(
-            [x - 1, x + 1], [slope * (x - 1) + intercept, slope * (x + 1) + intercept]
+            [-1.5, 1.5], [slope * (-1.5) + intercept, slope * 1.5 + intercept]
         )
     else:
         tangent_line.set_data([], [])
@@ -47,21 +46,27 @@ def reset_point(event):
 
 
 def toggle_tangent(label):
-    global show_tangent
     show_tangent = not show_tangent
 
 
 def change_line_style(label):
     line.set_linestyle(styles[label])
+    tangent_line.set_linestyle(styles[label])
     fig.canvas.draw_idle()
 
 
 def change_line_color(label):
     line.set_color(colors[label])
+    tangent_line.set_color(colors[label])
     fig.canvas.draw_idle()
 
 
-a, b, delta = 2, 3, np.pi / 2  # Параметры фигуры Лиссажу
+def update_delta(val):
+    delta = slider_delta.val
+    
+
+
+a, b, delta = 2, 3, np.pi / 3
 t_values = np.linspace(0, 2 * np.pi, 1000)
 x_values, y_values = lissajous(a, b, delta, t_values)
 
@@ -71,30 +76,30 @@ fig, ax = plt.subplots(figsize=(8, 6))
 (point,) = ax.plot([], [], "ro", markersize=8)
 (tangent_line,) = ax.plot([], [], "k--")
 
-# Инициализация параметров
 show_tangent = False
 
-# Размещение слайдера
 slider_ax_t = plt.axes([0.1, 0.01, 0.65, 0.03])
 slider_t = Slider(slider_ax_t, "Время", 0, 2 * np.pi, valinit=0, valstep=0.01)
 slider_t.on_changed(update_graph)
 
-# Размещение кнопки сброса
+slider_ax_delta = plt.axes([0.1, 0.06, 0.65, 0.03])
+slider_delta = Slider(
+    slider_ax_delta, "Delta", 0, 2 * np.pi, valinit=delta, valstep=0.01
+)
+slider_delta.on_changed(update_delta)
+
 reset_button_ax = plt.axes([0.8, 0.1, 0.1, 0.04])
 reset_button = Button(reset_button_ax, "Сброс")
 reset_button.on_clicked(reset_point)
 
-# Размещение флажка
 tangent_checkbox_ax = plt.axes([0.8, 0.025, 0.1, 0.04])
 tangent_checkbox = CheckButtons(tangent_checkbox_ax, ["Касательная"], actives=[False])
 tangent_checkbox.on_clicked(toggle_tangent)
 
-# Радиокнопки на изменение цвета линии
 radio_buttons_ax_color = plt.axes([0.8, 0.2, 0.1, 0.15])
 radio_buttons_color = RadioButtons(radio_buttons_ax_color, list(colors.keys()))
 radio_buttons_color.on_clicked(change_line_color)
 
-# Радиокнопки на изменение стиля линии
 radio_buttons_ax_style = plt.axes([0.8, 0.35, 0.1, 0.15])
 radio_buttons_style = RadioButtons(radio_buttons_ax_style, list(styles.keys()))
 radio_buttons_style.on_clicked(change_line_style)
